d64527cdee6e732ac7b667c36da53aaa8516908b
Bammez <Kingnuel4@gmail.com>
2024-05-04 09:42:38 +0200

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,18 @@
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    unimplemented!(
-        "Using the binary search algorithm, find the element '{key}' in the array '{array:?}' and return its index."
-    );
+use std::cmp::Ordering;
+
+pub fn find<T, S>(array: S, key: T) -> Option<usize>
+where
+    T: Ord,
+    S: AsRef<[T]>,
+{
+    let array = array.as_ref();
+    if array.is_empty() {
+        return None;
+    }
+    let half = array.len() / 2;
+    match array[half].cmp(&key) {
+        Ordering::Equal => Some(half),
+        Ordering::Greater => find(&array[..half], key),
+        Ordering::Less => find(&array[half + 1..], key),
+    }
 }
